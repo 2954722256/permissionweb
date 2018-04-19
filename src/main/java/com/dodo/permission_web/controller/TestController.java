8a7fd2a47462336fb6418e40eeea1897d8ec088a
dodo_lihao <2390183798@qq.com>
2018-04-19 16:10:58 +0800

    Interceptor
    对应拦截器的添加

--- src/main/java/com/dodo/permission_web/controller/TestController.java
+++ src/main/java/com/dodo/permission_web/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.dodo.permission_web.controller;
 
 
+import com.dodo.permission_web.common.ApplicationContextHelper;
 import com.dodo.permission_web.common.JsonData;
 import com.dodo.permission_web.dao.SysAclModuleMapper;
 import com.dodo.permission_web.exception.ParamException;
@@ -8,6 +9,7 @@ import com.dodo.permission_web.exception.PermissionException;
 import com.dodo.permission_web.model.SysAclModule;
 import com.dodo.permission_web.param.TestVo;
 import com.dodo.permission_web.utils.BeanValidator;
+import com.dodo.permission_web.utils.JsonMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -59,6 +61,24 @@ public class TestController {
         }
         throw new PermissionException("helloJsonEx ");
 //        return JsonData.success("test validate");
+    }
+
+
+    /**
+     * 用于测试，对应的 ApplicationContextHelper 和 Mapper 等工具类
+     * @param vo
+     * @return
+     * @throws ParamException
+     */
+    @RequestMapping("/validate2.json")
+    @ResponseBody
+    public JsonData validate2(TestVo vo) throws ParamException {
+        log.info("validate");
+        SysAclModuleMapper moduleMapper = ApplicationContextHelper.popBean(SysAclModuleMapper.class);
+        SysAclModule module = moduleMapper.selectByPrimaryKey(1);
+        log.info(JsonMapper.obj2String(module));
+        BeanValidator.check(vo);
+        return JsonData.success("test validate");
     }
 
 
